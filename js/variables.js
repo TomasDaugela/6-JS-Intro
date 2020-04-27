47ead0d47eaf9dea6ad85ced2d9a17afe80a71c3
Tomas <daugeltomas@gmail.com>
2020-04-27 14:11:14 +0300

    Initial commit and varaible init

---
+++ js/variables.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/* kintamuju tipai:
+    - const
+    - let
+    - var
+    */
+
+    const vardas = 'Tomas'; // tekstas
+    const akiuSkaicius = 2; // skaicius
+    const turiGalva = true; // logine reiksme : true / false
+    const mokaSkraidyti = false;
+    const manoPazymiai = []; // tuscias sarasas
+    const marytesPazymiai = [10, 8, 2]; // ne tuscias sarasas
+    const abc = ['a', 'b', 'c', 'd', 'e','f'] // tekstas sarasas
+    
+
+    console.log( vardas );
+    console.log( akiuSkaicius );
+    console.log( turiGalva );
+    console.log( mokaSkraidyti );
+    console.log( manoPazymiai );
+    console.log( marytesPazymiai );
+    console.log( abc );
+
+
+    let megstamasSkaicius = 19;
+    console.log( 'Megstamas skaicius:',megstamasSkaicius );
+
+    megstamasSkaicius = 3;
+    console.log( 'Megstamas skaicius:',megstamasSkaicius );
+
+    megstamasSkaicius = 7;
+    console.log( 'Megstamas skaicius:',megstamasSkaicius );
